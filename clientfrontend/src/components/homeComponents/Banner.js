efdac733a3a50ef0e908993fcd7a1e24e74e2435
IsanJoe <nessanjoeipulan@gmail.com>
2023-01-13 17:14:00 +0800

    'Added banner, footer'

---
+++ clientfrontend/src/components/homeComponents/Banner.js
@@ -0,0 +1,15 @@
+import Video2 from "../video/Video2.mp4";
+
+
+const Banner = () => {
+    return(
+      <>
+        <video className="w-100" src={Video2} autoPlay loop muted ></video>
+        <div className="text-center" style={{marginTop:'8vmin', marginBottom:"4vmin"}}>
+          <p className="fw-semibold" style={{fontSize:"7vmin"}}>Limited Edition</p>
+        </div>
+      </>
+    );
+  }
+
+  export default Banner;
